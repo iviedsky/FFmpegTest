102918c1039ae794408e587bdf0322b443ad9b9a
chensisheng <sisheng.chen@zhenai.com>
2015-08-17 11:33:12 +0800

    集成了kxmovie的功能

--- FFmpeg/FFmpeg/ViewController.m
+++ FFmpeg/FFmpeg/ViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ViewController.h"
-#import "WFFFmpegWraper.h"
+#import "KxMovieViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) NSArray *localMovies;
+@property (nonatomic, strong) NSArray *remoteMovies;
+@property (strong, nonatomic) UITableView *tableView;
 
 @end
 
@@ -18,13 +22,180 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [WFFFmpegWraper makeVideo];
-
+    self.remoteMovies = @[
+                      @"http://photo13.zastatic.com/images/voice/16140/64552310/1439539909910.mp4",
+                      @"http://photo13.zastatic.com/images/voice/16140/64552310/1439781836123.mp4"
+                      ];
+    
+    self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    self.tableView.backgroundColor = [UIColor whiteColor];
+    //self.tableView.backgroundView = [[UIImageView alloc] initWithImage:image];
+    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleBottomMargin;
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    
+    [self.view addSubview:self.tableView];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    [self reloadMovies];
+    [self.tableView reloadData];
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+- (void) reloadMovies
+{
+    NSMutableArray *ma = [NSMutableArray array];
+    NSFileManager *fm = [[NSFileManager alloc] init];
+    NSString *folder = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                                            NSUserDomainMask,
+                                                            YES) lastObject];
+    NSArray *contents = [fm contentsOfDirectoryAtPath:folder error:nil];
+    
+    for (NSString *filename in contents) {
+        
+        if (filename.length > 0 &&
+            [filename characterAtIndex:0] != '.') {
+            
+            NSString *path = [folder stringByAppendingPathComponent:filename];
+            NSDictionary *attr = [fm attributesOfItemAtPath:path error:nil];
+            if (attr) {
+                id fileType = [attr valueForKey:NSFileType];
+                if ([fileType isEqual: NSFileTypeRegular] ||
+                    [fileType isEqual: NSFileTypeSymbolicLink]) {
+                    
+                    NSString *ext = path.pathExtension.lowercaseString;
+                    
+                    if ([ext isEqualToString:@"mp3"] ||
+                        [ext isEqualToString:@"caff"]||
+                        [ext isEqualToString:@"aiff"]||
+                        [ext isEqualToString:@"ogg"] ||
+                        [ext isEqualToString:@"wma"] ||
+                        [ext isEqualToString:@"m4a"] ||
+                        [ext isEqualToString:@"m4v"] ||
+                        [ext isEqualToString:@"wmv"] ||
+                        [ext isEqualToString:@"3gp"] ||
+                        [ext isEqualToString:@"mp4"] ||
+                        [ext isEqualToString:@"mov"] ||
+                        [ext isEqualToString:@"avi"] ||
+                        [ext isEqualToString:@"mkv"] ||
+                        [ext isEqualToString:@"mpeg"]||
+                        [ext isEqualToString:@"mpg"] ||
+                        [ext isEqualToString:@"flv"] ||
+                        [ext isEqualToString:@"vob"]) {
+                        
+                        [ma addObject:path];
+                    }
+                }
+            }
+        }
+    }
+    
+    // Add all the movies present in the app bundle.
+    NSBundle *bundle = [NSBundle mainBundle];
+    [ma addObjectsFromArray:[bundle pathsForResourcesOfType:@"mp4" inDirectory:@"SampleMovies"]];
+    [ma addObjectsFromArray:[bundle pathsForResourcesOfType:@"mov" inDirectory:@"SampleMovies"]];
+    [ma addObjectsFromArray:[bundle pathsForResourcesOfType:@"m4v" inDirectory:@"SampleMovies"]];
+    [ma addObjectsFromArray:[bundle pathsForResourcesOfType:@"wav" inDirectory:@"SampleMovies"]];
+    
+    [ma sortedArrayUsingSelector:@selector(compare:)];
+    
+    _localMovies = [ma copy];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:     return @"Remote";
+        case 1:     return @"Local";
+    }
+    return @"";
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:     return _remoteMovies.count;
+        case 1:     return _localMovies.count;
+    }
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:cellIdentifier];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    NSString *path;
+    
+    if (indexPath.section == 0) {
+        
+        path = _remoteMovies[indexPath.row];
+        
+    } else {
+        
+        path = _localMovies[indexPath.row];
+    }
+    
+    cell.textLabel.text = path.lastPathComponent;
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *path;
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    
+    if (indexPath.section == 0) {
+        
+        if (indexPath.row >= _remoteMovies.count) return;
+        path = _remoteMovies[indexPath.row];
+        
+    } else {
+        
+        if (indexPath.row >= _localMovies.count) return;
+        path = _localMovies[indexPath.row];
+    }
+    
+    // increase buffering for .wmv, it solves problem with delaying audio frames
+    if ([path.pathExtension isEqualToString:@"wmv"])
+        parameters[KxMovieParameterMinBufferedDuration] = @(5.0);
+    
+    // disable deinterlacing for iPhone, because it's complex operation can cause stuttering
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+        parameters[KxMovieParameterDisableDeinterlacing] = @(YES);
+    
+    // disable buffering
+    //parameters[KxMovieParameterMinBufferedDuration] = @(0.0f);
+    //parameters[KxMovieParameterMaxBufferedDuration] = @(0.0f);
+    
+    KxMovieViewController *vc = [KxMovieViewController movieViewControllerWithContentPath:path
+                                                                               parameters:parameters];
+    [self presentViewController:vc animated:YES completion:nil];
+    //[self.navigationController pushViewController:vc animated:YES];
+    
 }
 
 @end
